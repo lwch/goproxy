b95a1be2a8b559d69ba282e5cf4b171702068300
lwch <lwch748@gmail.com>
2023-03-29 11:52:14 +0800

    增加log

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"context"
 	"io"
+	"os"
 	"time"
 
 	"github.com/lwch/logging"
@@ -22,7 +23,7 @@ func New(dir string, timeout time.Duration) *Cache {
 
 func (c *Cache) Get(ctx context.Context, name string) (io.ReadCloser, error) {
 	logging.Info("GET: %s", name)
-	return nil, nil
+	return nil, os.ErrNotExist
 }
 
 func (c *Cache) Set(ctx context.Context, name string, content io.ReadSeeker) error {
--- internal/cmd/run.go
+++ internal/cmd/run.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/goproxy/goproxy"
+	"github.com/lwch/logging"
 	"github.com/lwch/runtime"
 )
 
@@ -17,6 +18,7 @@ func Run() {
 		ProxiedSUMDBs: []string{"sum.golang.org", "sum.golang.google.cn"},
 		Cacher:        cache.New(cacheDir, cacheTimeout),
 	}
+	logging.Info("goproxy listen on %d", listen)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", listen), proxy)
 	runtime.Assert(err)
 }
