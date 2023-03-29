e4978c77393d26bf9befcc91146e2ac42f5b0d36
lwch <lwch748@gmail.com>
2023-03-29 15:12:15 +0800

    支持@latest标签

--- internal/storage/local/local.go
+++ internal/storage/local/local.go
@@ -29,16 +29,18 @@ func New(indexer *index.Indexer, dir string, timeout time.Duration) storage.Stor
 
 func (l *Local) Get(ctx context.Context, name string) (io.ReadCloser, error) {
 	logging.Info("GET: %s", name)
-	if strings.HasSuffix(name, "/@v/list") {
-		return l.Base.Get(ctx, strings.TrimSuffix(name, "/@v/list"))
+	if strings.HasSuffix(name, "/@v/list") ||
+		strings.HasSuffix(name, "/@latest") {
+		return l.Base.Get(ctx, name)
 	}
 	return nil, os.ErrNotExist
 }
 
 func (l *Local) Set(ctx context.Context, name string, content io.ReadSeeker) error {
 	logging.Info("SET: %s", name)
-	if strings.HasSuffix(name, "/@v/list") {
-		return l.Base.Save(strings.TrimSuffix(name, "/@v/list"), content)
+	if strings.HasSuffix(name, "/@v/list") ||
+		strings.HasSuffix(name, "/@latest") {
+		return l.Base.Save(name, content)
 	}
 	data, err := io.ReadAll(content)
 	if err != nil {
